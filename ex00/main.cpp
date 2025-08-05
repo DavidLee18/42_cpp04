b0114fe968ca6d3c779685b5daa2c0e74a1c802f
Jaehyun Lee <jaehylee@student.42gyeongsan.kr>
2025-08-05 23:54:05 +0900

    add `WrongAnimal` and `WrongCat`

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,20 +1,31 @@
 #include "Dog.h"
 #include "Cat.h"
+#include "WrongCat.h"
 
 int main() {
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	const WrongAnimal* weta = new WrongAnimal();
+	const WrongAnimal* kat = new WrongCat();
+
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	std::cout << meta->getType() << " " << std::endl;
+	std::cout << weta->getType() << " " << std::endl;
+	std::cout << kat->getType() << " " << std::endl;
+
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
+	weta->makeSound();
+	kat->makeSound();
 
 	delete i;
 	delete j;
 	delete meta;
+	delete weta;
+	delete kat;
 
 	return 0;
 }
